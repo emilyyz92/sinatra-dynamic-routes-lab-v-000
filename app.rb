9c42f95d0091fcc72bc5f9989fea268d09116cb7
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-09 17:04:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,9 +13,8 @@ class App < Sinatra::Base
 
   get "/say/:number/:phrase" do
     n = params[:number].to_i
-    a = ""
     n.times do
-      a += "#{params[:phrase]}\n"
+      a = "#{params[:phrase]}\n"
     end
     a
   end
