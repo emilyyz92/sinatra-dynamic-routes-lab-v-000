13d185032e6bca7f50633f11466e07b40f01bf9d
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-09 20:22:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
     operation = params[:operation].to_s
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
+    binding.pry
     if operation == "add"
       answer = number1 + number2
     elsif operation == "subtract"
